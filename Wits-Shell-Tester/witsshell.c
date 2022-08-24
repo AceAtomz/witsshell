d8b059051643ee0acbbb4a59bddb68d19053d0a6
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-08-24 19:27:32 +0200

    input and exit works

--- Wits-Shell-Tester/witsshell.c
+++ Wits-Shell-Tester/witsshell.c
@@ -7,9 +7,89 @@
 #include <assert.h>
 #include <fcntl.h>
 
+void witsshell();
+void batchmode(char *MainArgv);
+void storeCommand(char* CommandLine);
+void excecuteCommand(char *MainArgv);
+int check_for_EOF();
+
+#define LENGTH 15
+
+char *commands[LENGTH];
+int ncom = 0;
+char* exitString = "exit";
+int exitInt = 1;
 
 int main(int MainArgc, char *MainArgv[]){
-	printf("Arguments: ");
+	if(MainArgc == 1){
+		witsshell();
+	}else if(MainArgc == 2){
+		batchmode(MainArgv[1]);
+	}else{
+		printf("Invalid number of arguments. Must be either 0 (./witsshell) or 1 (./witsshell batch.txt)");
+	}
 
 	return(0);
+}
+
+void witsshell(){
+	char *buffer = NULL;
+    size_t bufsize = 32;
+    //size_t characters;
+	do {
+		printf("witsshell> ");
+
+		if(check_for_EOF()){
+			printf("^D\n");
+			exit(0);
+		}
+		
+		getline(&buffer,&bufsize,stdin);
+		buffer[strcspn(buffer, "\n")] = 0;
+		
+		//getline(&buffer,&bufsize,stdin); //characters = getline(&buffer,&bufsize,stdin);
+		storeCommand(buffer);
+
+		for(int i=0;i<ncom;i++){
+			printf("%s\n", commands[i]);
+		}
+	}while(exitInt != 0);
+	free(buffer);
+	exit(0);
+}
+
+void batchmode(char *MainArgv){
+	printf("Opening %s ", MainArgv);
+	//int temp = strstr(MainArgv[0], ".txt"); //!=NULL
+}
+
+void storeCommand(char* CommandLine){
+	//memset(commands, 0, LENGTH);
+	int i = 0;
+
+	char * token = strtok(CommandLine, " ");
+	exitInt = strcmp(token,exitString);
+	if(exitInt==0){
+		exit(0);
+	}
+
+   	// loop through the string to extract all other tokens
+   	while( token != NULL ) {
+      	commands[i] = token;
+      	token = strtok(NULL, " ");
+		i++;
+   	}
+	ncom = i;
+}
+
+void excecuteCommand(char *MainArgv){
+	printf("Excecuting %s ", MainArgv);
+}
+
+int check_for_EOF(){
+    if (feof(stdin)) return 1;
+    int c = getc(stdin);
+    if (c == EOF) return 1;
+    ungetc(c, stdin);
+	return 0;
 }
