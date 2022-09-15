316e429e0eac9e16022a9e9ba46b724c63d5d8fe
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-09-15 17:42:49 +0200

    yes

--- Wits-Shell-Tester/witsshell.c
+++ Wits-Shell-Tester/witsshell.c
@@ -15,7 +15,9 @@ void exec(char* commands[]);
 void excecutels(char* commands[]);
 void excecutecd(char* commands[]);
 void excecutePath(char* commands[]);
+void excecuteSH(char* commands[]);
 int check_for_EOF();
+bool haveAccess(char* command);
 
 #define LENGTH 15
 #define PATH "/bin/"
@@ -45,17 +47,23 @@ void witsshell(){
     size_t bufsize = 32;
 	while(true){
 		printf("witsshell> ");
-
+		
 		if(check_for_EOF()){  //if CTRL-D then exit gracefully
-			//printf("^D\n");
 			exit(0);
 		}
-		
+
 		getline(&buffer,&bufsize,stdin);
+
+		if(!strcmp(buffer, "\n")){
+			continue;
+		}
+
 		buffer[strcspn(buffer, "\n")] = 0;
-	
-		storeCommand(buffer);
-		excecuteCommand(commands);
+
+		allCommands[0] = buffer;
+		nAllCom = 1;
+		
+		excecuteCommand(allCommands);
 	}
 	free(buffer);
 }
@@ -109,7 +117,7 @@ void excecuteCommand(char *Allcommands[]){
 		storeCommand(Allcommands[i]);
 		
 		if(!strcmp(commands[0], "ls")){
-		excecutels(commands);
+			excecutels(commands);
 		}else if(!strcmp(commands[0],"exit")){
 			if(ncom==1){
 				exit(0);
@@ -127,7 +135,10 @@ void excecuteCommand(char *Allcommands[]){
 			excecutecd(commands);
 		}else if(!strcmp(commands[0], "path")){
 			excecutePath(commands);
-		}else{
+		}else if(haveAccess(commands[0])){
+			excecuteSH(commands);
+		}
+		else{
 			char error_message[30] = "An error has occurred\n";
 			write(STDERR_FILENO, error_message, strlen(error_message));
 		}
@@ -157,6 +168,7 @@ void excecutels(char* commands[]){
 		}
 	}else{ //if(fork()==0) if you dont wanna close witsshell after ls
 		exec(commands);
+		//execv("/bin/ls", commands);
 	}
 }
 
@@ -181,14 +193,30 @@ void excecutecd(char* commands[]){
 }
 
 void exec(char* commands[]){ 
-	char run[50];
-	strcat(run, currPath[0]); //run = PATH = /bin/
-	strcat(run, commands[0]); //run = PATH + commands[0] ie. /bin/ls
-	execv(run, commands);	//runs command through execv
+	//pid_t cpid;
+    if (fork()== 0){
+		int i=0;
+		while(currPath[i]!=0){
+			char run[50] = "";
+			strcat(run, currPath[i]); //run = PATH = /bin/
+			strcat(run, commands[0]); //run = PATH + commands[0] ie. /bin/ls
+			if(!access(run, F_OK))
+				execv(run, commands);	//runs command through execv
+			else{
+				char error_message[30] = "An error has occurred\n";
+				write(STDERR_FILENO, error_message, strlen(error_message));
+			}
+			i++;
+		}
+		exit(0);           /* terminate child */
+	}else
+        wait(NULL); /* reaping parent */
+
+	
 }
 
 void excecutePath(char* commands[]){
-	if(ncom==0){
+	if(ncom==1){
 		memset(currPath, 0, sizeof(currPath));
 	}else{
 		for(int i=0;i<ncom-1;i++){
@@ -198,7 +226,24 @@ void excecutePath(char* commands[]){
 }
 
 void excecuteSH(char* commands[]){
-	for(int i=0;i<sizeof(currPath);i++){ //TODO: move this loop to exec 
-		exec(); 
+	char* temp = commands[0];
+	commands[0] = "sh";
+	commands[1] = temp;
+	
+	exec(commands);
+}
+
+bool haveAccess(char* command){
+	int i=0;
+	while(currPath[i]!=0){
+		int a;
+		char run[50] = "";
+		strcat(run, currPath[i]); //run = PATH = /bin/
+		strcat(run, command); //run = PATH + commands[0] ie. /bin/ls
+		printf("%s", run);
+		a = access(run, F_OK);
+		if(a==0) return true;
+		i++;
 	}
+	return false;
 }
